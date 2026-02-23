6d387b37d9fbb4121530fd5cc75bd67940d9979f
salvagd09 <salva6770@gmail.com>
2026-02-23 18:28:35 -0500

    Registros de empleados

---
+++ Querys_BD/Inserts.sql
@@ -0,0 +1,5 @@
+USE shaddaiSystem;
+Select * from roles;
+INSERT INTO Usuarios(Nombres,Apellidos,Contrasena,Rol,username) VALUES ("Salvador Vasco","Goicochea Díaz","1234510",1,"salvagd09"),("Jhossel Bryan","Paredes Rojas","abcde59",2,"jhossel2510"),
+("Álvaro Ian","Ávalos Alva","Alva19",3,"alvaAva22");
+
