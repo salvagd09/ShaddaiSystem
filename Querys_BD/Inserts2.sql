324ee6f1e8918500cb7c1d9311f36716e60f2dfd
JhosselPR <153229232+JhosselPR@users.noreply.github.com>
2026-02-24 14:41:23 -0500

    Update Inserts2.sql

--- Querys_BD/Inserts2.sql
+++ Querys_BD/Inserts2.sql
@@ -1,1 +1,7 @@
-
+USE shaddaiSystem;
+INSERT INTO Categoria (nombre_Categoria) VALUES ('Descartables'), ('Bolsas');
+INSERT INTO Producto (Codigo_Producto, Nombre, Stock_Tienda, Stock_Almacen, Unidad_Medida, Precio, Stock_Minimo, ID_Categoria)
+VALUES 
+('PROD-001', 'Vasos de Plástico 12oz', 50, 200, 'Paquete', 5.50, 20, 1),
+('PROD-002', 'Platos Descartables Nro 8', 30, 150, 'Ciento', 8.20, 15, 1),
+('PROD-003', 'Bolsas de Basura 50x70', 100, 500, 'Millar', 15.00, 50, 2);
